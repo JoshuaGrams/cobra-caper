96503d004e2c0c436acb99cdd1f424b73e726287
Josh Grams <josh@qualdan.com>
2016-12-09 17:54:22 -0500

    Minor lib changes, add Inconsolata font, set blend mode.

--- main.js
+++ main.js
@@ -1,12 +1,37 @@
 (function() {
 	var glCtx = WebGL.getContext('main');
+	glCtx.blendFunc(glCtx.SRC_ALPHA, glCtx.ONE_MINUS_SRC_ALPHA);
+	glCtx.enable(glCtx.BLEND);
 
-	var initialState = {};
-	var game = new GameMode(initialState, glCtx.canvas, 1/100);
+	var textures = LoadImage('textures.png', function() {
 
-	game.draw = function(state) {
-		WebGL.clear(glCtx, [0.03, 0.03, 0.1, 1]);
-	}
+		var textureCoords = {
+			inconsolata: { x: 0, y: 0, w: 384, h: 78, img: textures }
+		};
 
-	game.go();
+		var ui = new TexturedLayer(glCtx, textures);
+		var uiFont = ui.font().addCharacters(32, 127, 32, 3,
+			textureCoords.inconsolata);
+
+		var initialState = {
+			vp: new Viewport(glCtx, [0, 0], 16*9),
+			ui: ui, uiFont: uiFont
+		};
+		var game = new GameMode(initialState, glCtx.canvas, 1/100);
+
+		game.draw = function(state) {
+			WebGL.clear(glCtx, [0.5, 0.5, 0.3, 1]);
+			if(!state.title) {
+				state.title = state.uiFont.drawText([100, 100], [1, 0],
+					"Some Text Here");
+			} else {
+				for(var i=0; i<state.title.length; ++i) state.title[i].draw();
+			}
+			ui.draw(state.vp.pixelMatrix());
+		}
+
+		game.fixedStep = function(state, ds) { return state; }
+
+		game.go();
+	});
 })();
